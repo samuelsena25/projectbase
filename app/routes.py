24ef943b0368ba98f5bda1a68464a379a7c54c86
Samuel Sena <aldebaran.informatica2@gmail.com>
2026-01-16 21:36:16 -0300

    The fetch functionality has been completed.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify9
+from flask import Blueprint, render_template, request, jsonify
 
 main = Blueprint("main", __name__)
 
@@ -9,3 +9,24 @@ def home():
 @main.route('/acaiprime')
 def acaiprime():
     return render_template('acaiprime.html')
+
+@main.route('/calcular', methods=['POST'])
+def calcular():
+    data = request.get_json()
+
+    total_value = data['total_value']
+    total_orders = data['total_orders']
+    my_deliveries = data['my_deliveries']
+    maria_orders = data['maria_orders']
+
+    estimated_profit = total_orders * 5
+    my_payment = my_deliveries * 8
+    marias_payment = maria_orders * 3
+    final_billing = total_value - my_payment - marias_payment
+
+    return jsonify({
+        "estimated_profit": estimated_profit,
+        "my_payment": my_payment,
+        "marias_payment": marias_payment,
+        "final_billing": final_billing
+    })
--- static/script.js
+++ static/script.js
@@ -1,27 +1,40 @@
 
-document.getElementById("btnCalcular").addEventListener("click", () => {
-    const payload = {
-        total_value: parseInt(document.getElementById("form_total_value").value),
-        total_orders: parseInt(document.getElementById("form_total_orders").value),
-        my_deliveries: parseInt(document.getElementById("form_my_deliveries").value),
-        maria_orders: parseInt(document.getElementById("form_maria_orders").value),
-    };
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const btn = document.getElementById("btnCalcular");
 
-    fetch("/calcular", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(payload)
-    })
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById("resultSection").innerHTML = `
-            <div>Lucro estimado: R$ ${data.estimated_profit}</div>
-            <div>Meu pagamento: R$ ${data.my_payment}</div>
-            <div>Pagamento da Maria: R$ ${data.marias_payment}</div>
-            <div>Faturamento final da loja: R$ ${data.final_billing}</div>
-        `
-    })
-    .catch(() => {
-        document.getElementById("resultSection").innerHTML = "<div>Erro ao calcular.</div>";
+    if (!btn) {
+        console.ward("Botão #btn_calc não encontrado.");
+        return
+    }
+
+    btn.addEventListener("click", () => {
+        
+        const payload = {
+            total_value: parseInt(document.getElementById("form_total_value").value),
+            total_orders: parseInt(document.getElementById("form_total_orders").value),
+            my_deliveries: parseInt(document.getElementById("form_my_deliveries").value),
+            maria_orders: parseInt(document.getElementById("form_maria_orders").value),
+        };
+
+        fetch("/calcular", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(payload)
+        })
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("resultPart1").innerText = `Lucro estimado:
+                Meu pagamento:
+                Pagamento da Maria:
+                Faturamento final da loja:`
+            document.getElementById("resultPart2").innerText = `R$ ${data.estimated_profit}
+                R$ ${data.my_payment}
+                R$ ${data.marias_payment}
+                R$ ${data.final_billing}`
+        })
+        .catch(() => {
+            document.getElementById("resultPart1").innerHTML = "<div>Erro ao calcular.</div>";
+        });
     });
 });
